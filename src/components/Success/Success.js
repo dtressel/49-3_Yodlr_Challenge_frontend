7bf0d6e0c0b7ecb0055f0b86ad17578041710c2a
Daniel Tressel <dhtressel@gmail.com>
2023-08-15 22:26:24 -0400

    new success page

--- src/components/Success/Success.js
+++ src/components/Success/Success.js
@@ -1,27 +1,49 @@
-import { useLocation } from "react-router-dom";
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from "react-router-dom";
+import { Link as ReactRouterLink } from "react-router-dom";
+import Button from '@mui/material/Button';
 import './Success.css';
 
 const Success = () => {
   const { state: user } = useLocation();
+  const navigate = useNavigate();
+  console.log(user);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/signup");
+    }
+  }, [user, navigate]);
 
   return (
     <>
-      <h2>New User Registered!</h2>
-      <h3>User:</h3>
-      <div className="Success-user-card">
-        <div>
-          <div>First Name</div>
-          <div>{user.firstName}</div>
+      {user && 
+        <div className="Success">
+          <h2>New User Registered!</h2>
+          <div className="Success-user-card-wrapper">
+            <div className="Success-user-card">
+              <h3>User:</h3>
+              <div className="Success-user-card-row">
+                <div>First Name</div>
+                <div>{user.firstName}</div>
+              </div>
+              <div className="Success-user-card-row">
+                <div>Last Name</div>
+                <div>{user.lastName}</div>
+              </div>
+              <div className="Success-user-card-row">
+                <div>Email</div>
+                <div>{user.email}</div>
+              </div>
+              <div className="Success-user-card-row">
+                <div>Status</div>
+                <div>{user.state}</div>
+              </div>
+            </div>
+          </div>
+          <Button component={ReactRouterLink} to='/'>Return to Home</Button>
         </div>
-        <div>
-          <div>Last Name</div>
-          <div>{user.lastName}</div>
-        </div>
-        <div>
-          <div>Email</div>
-          <div>{user.email}</div>
-        </div>
-      </div>
+      }
     </>
   )
 }
