da8644c794a991bffa53a778942a5eb84958ced0
Daniel Tressel <dhtressel@gmail.com>
2023-08-15 22:25:41 -0400

    base of Admin page

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,5 +1,28 @@
-const Admin = () => {
+import { useEffect, useState } from 'react';
+import ApiLink from '../../helpers/ApiLink';
+import UsersTable from '../UsersTable/UsersTable';
 
+const Admin = () => {
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const res = await ApiLink.getAllUsers();
+      setUsers(res.data);
+    }
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      <h2>Users</h2>
+      {users && 
+        <UsersTable users={users} />
+      }
+
+    </>
+
+  )
 }
 
 export default Admin;
