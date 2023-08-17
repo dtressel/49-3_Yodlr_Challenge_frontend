a391dc2f6785b31ac8c8af9f44c4f1579de80cfc
Daniel Tressel <dhtressel@gmail.com>
2023-08-17 09:42:49 -0400

    added note about backend

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,6 +10,12 @@ const Home = () => {
       <div>
         Thank you for reviewing my completion of Yodlr's design challenge! I will be using this home page to outline how I completed this challenge and what features I included. Use the links in the navbar to visit the required signup and admin pages.
       </div>
+      <h4>Backend</h4>
+      <ul>
+        <li>Used the supplied backend</li>
+        <li>Use with client-side rendering</li>
+        <li>One modification: Installed and used the cors NPM package to allow Cross-Origin Resource Sharing</li>
+      </ul>
       <h4>Technologies Used (Frontend)</h4>
       <ul>
         <li>React</li>
